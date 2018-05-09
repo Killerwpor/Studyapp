70e0693b8ec61c840e3027fbf4032b1904ca0987
killerwp <johanc.suarez@hotmail.com>
2018-05-08 20:25:44 -0500

    "Login con fb hecho"

---
+++ app/src/main/java/co/edu/udea/studyapp/Login.java
@@ -0,0 +1,60 @@
+package co.edu.udea.studyapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+public class Login extends AppCompatActivity {
+
+    Button botonLogin;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        botonLogin=findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callbackManager = CallbackManager.Factory.create();
+
+                LoginManager.getInstance().registerCallback(callbackManager,
+                        new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                // App code
+                            }
+
+                            @Override
+                            public void onError(FacebookException exception) {
+                                // App code
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
--- app/src/main/java/co/edu/udea/studyapp/MainActivity.java
+++ app/src/main/java/co/edu/udea/studyapp/MainActivity.java
@@ -1,5 +1,6 @@
 package co.edu.udea.studyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,6 +84,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent (getApplicationContext(), Login.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
 
